363a47881bdcbdd6e313963d5288e8a3c53d2a51
farazhashmi21 <farazhashmi9000@hotmail.com>
2024-03-19 18:33:39 +0000

    Permissions Update;

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en-US">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Full Width Pics - Start Bootstrap Template</title>
+        <title>{{ config('app.name') }} - Laravel Based Web Application</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('start/assets/favicon.ico') }}" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{ asset('start//css/styles.css') }}" rel="stylesheet" />
         <script src="{{ asset('start/js/scripts.js') }}"></script>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
     </head>
     <body onload="displ();">
         <!-- Responsive navbar-->
@@ -20,10 +23,10 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="/" title="Home">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about" title="About">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="services" title="Services">Services</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact" title="Contact">Contact</a></li>
                     </ul>
                 </div>
             </div>
@@ -37,13 +40,9 @@
             </div>
         </header>
         @yield("content")
-        
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; {{ config('app.name') }} <script>document.write(new Date().getFullYear());</script></p></div>
         </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
     </body>
 </html>
